17e715649b4db5568fa244c425b6e96ef2c07e57
Etkan Zawad <76504341+etkanzawad@users.noreply.github.com>
2023-09-07 11:50:02 +0100

    Update Main.java

--- src/javamail/Main.java
+++ src/javamail/Main.java
@@ -2,6 +2,6 @@ package javamail;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        JavaMail.sendMail("etkanzawad123@gmail.com");
+        JavaMail.sendMail(".com");
     }
 }
